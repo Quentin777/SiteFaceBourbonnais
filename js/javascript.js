d504d1a713fbf265d30f37719f3686d869e5d7db
Ansion Quentin <ansion.quentin@outlook.fr>
2017-01-30 11:22:35 +0100

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -5,3 +5,19 @@ $(window).scroll(function() {
     $('nav').removeClass('shrink');
   }
 });
+
+
+$(document).ready(function() {
+  //$('h1').css({color: 'red'})
+  $('li img').on('click', function () {
+        var src = $(this).attr('src');
+        var img = '<img src="' + src + '" class="img-responsive"/>';
+        $('#myModal').modal();
+        $('#myModal').on('shown.bs.modal', function () {
+            $('#myModal .modal-body').html(img);
+        });
+        $('#myModal').on('hidden.bs.modal', function () {
+            $('#myModal .modal-body').html('');
+        });
+    });
+});
